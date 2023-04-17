d31730c1eefb23d68e4eb1ee9c484ba1c4b024ce
D'Raven McCoy <dravenmccoy1@gmail.com>
2023-04-16 23:01:39 -0500

    Made majority of the Alarm Screen
    Just need to implement a date selector

--- lib/Alarm.dart
+++ lib/Alarm.dart
@@ -17,23 +17,35 @@ class AddAlarm extends StatefulWidget {
   @override
   State<AddAlarm> createState() => _AddAlarmState();
 }
+enum NotificationType {alarm, notification}
 
 class _AddAlarmState extends State<AddAlarm> {
-  int _counter = 0;
+  NotificationType? _notificationType = NotificationType.alarm;
+  bool isRepeatable = false;
+  bool isSunday = false;
+  bool isMonday = false;
+  bool isTuesday = false;
+  bool isWednesday = false;
+  bool isThursday = false;
+  bool isFriday = false;
+  bool isSaturday = false;
+  TimeOfDay selectedTime = TimeOfDay.now();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> dayPicker() async {
+    TimeOfDay?  pickedTime = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
   }
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -47,8 +59,7 @@ class _AddAlarmState extends State<AddAlarm> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
@@ -64,23 +75,80 @@ class _AddAlarmState extends State<AddAlarm> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
+
+             Text(
+              'Type',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ListTile(
+                title: const Text("Alarm"),
+              leading: Radio<NotificationType>(
+                value: NotificationType.alarm,
+                groupValue: _notificationType,
+                onChanged: (NotificationType? value) {
+                  setState(() {
+                    _notificationType = value;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text("Notification"),
+              leading: Radio<NotificationType>(
+                value: NotificationType.notification,
+                groupValue: _notificationType,
+                onChanged: (NotificationType? value) {
+                  setState(() {
+                    _notificationType = value;
+                  });
+                },
+              ),
+            ),
+        CheckboxListTile(
+          title: const Text("Repeatable?"),
+          checkColor: Colors.white,
+          value: isRepeatable,
+          onChanged: (bool? value) {
+            setState(() {
+              isRepeatable = value!;
+            });
+          },
+         ),
+            ElevatedButton(
+                onPressed: dayPicker , child: const Text("Pick Time")),
+            Text("$selectedTime"),
+          ],
+
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+
+          children: <Widget>[
+            FloatingActionButton.extended(
+              isExtended: true,
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              tooltip: 'Cancel',
+              label: const Text("Cancel"),
+
+            ),
+            FloatingActionButton.extended(
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              tooltip: 'Save',
+              label: const Text("Save"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
